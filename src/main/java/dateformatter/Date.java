d67e685668721dfda8b1e24b439db2bf58d0f28a
Johannes-Baischer <Johannes.Baischer@gmail.com>
2022-10-12 20:03:28 +0200

    20221012 isValid checks + FileIO
    Date.java
     - added better validity checks
    
    DateMain.java
     - added FileIO
     - added checks if date exists
    
     Input.txt
     - added test input file
    
     Output.txt
     - test output file

--- src/main/java/dateformatter/Date.java
+++ src/main/java/dateformatter/Date.java
@@ -2,14 +2,14 @@ package dateformatter;
 
 public class Date {
     //date variables
-    private int day;
-    private int month;
-    private int year;
+    private String day;
+    private String month;
+    private String year;
 
     //invalid flag
     private boolean isValid;
 
-    public Date(int day, int month, int year){
+    public Date(String day, String month, String year){
         setDay(day);
         setMonth(month);
         setYear(year);
@@ -26,55 +26,144 @@ public class Date {
         String[] dateComponents = dateString.split("[\\/\\-.]");
 
         if(dateComponents.length != 3){
+            //dateString invalid
             setIsValid(false);
             return;
         }
 
-        for(int i = 0; i < formatCode.length(); i++){
-            char c = formatCode.charAt(i);
+        if((formatCode.length() != 3) && (formatCode.length() != 4)){
+            //length of formatcode invalid
+        }
 
+        //---------------ORDER
+        //Possible order character: A, B, C
+        char c = formatCode.charAt(0);
+        switch (c){
+            case 'A':
+                setMonth(dateComponents[0]);
+                setDay(dateComponents[1]);
+                setYear(dateComponents[2]);
+                break;
+
+            case 'B':
+                setYear(dateComponents[0]);
+                setMonth(dateComponents[1]);
+                setDay(dateComponents[2]);
+                break;
+
+            case 'C':
+                setDay(dateComponents[0]);
+                setMonth(dateComponents[1]);
+                setYear(dateComponents[2]);
+                break;
+
+            default:
+                //no valid order character found
+                setIsValid(false);
+                return;
+        }
+
+        //---------------SEPERATOR
+        //Possible seperators: /, ., -
+        c = formatCode.charAt(1);
+        switch (c){
+            case '-':
+            case '/':
+            case '.':
+                int count = dateString.length() - dateString.replace(c + "", "").length();
+                if(count != 2){
+                    //seperator isn't found twice
+                    setIsValid(false);
+                    return;
+                }
+                break;
+
+            default:
+                //no valid seperator found
+                setIsValid(false);
+                return;
+        }
+
+        //---------------YEAR FORMAT
+        //Possible year formats: 2, 4
+        
+        c = formatCode.charAt(2);
+        switch (c){
+            case '2':
+                //length(DD.MM.YY) == 8
+                if(dateString.length() != 8){
+                    setIsValid(false);
+                    return;
+                }
+                break;
+
+            case '4':
+                //length(DD.MM.YYYY) == 10
+                if(dateString.length() != 10){
+                    setIsValid(false);
+                    return;
+                }
+                break;
+
+            default:
+                //no valid year format found
+                setIsValid(false);
+                return;
+        }
+
+        //---------------BLANK
+        if(formatCode.length() == 3){
+            //does not contain "b" blank option
+
+            for(String component : dateComponents){
+                if(component.contains(" ")){
+                    //contains blank without b option
+                    setIsValid(false);
+                    return;
+                }
+            }
+        }
+        else{
+            //contains "b" blank option <=> lenght == 4
+
+            //Possible options: b
+            c = formatCode.charAt(3);
             switch (c){
-                case 'A':
-                    setMonth(Integer.parseInt(dateComponents[0]));
-                    setDay(Integer.parseInt(dateComponents[1]));
-                    setYear(Integer.parseInt(dateComponents[2]));
-                    break;
+                case 'b':
+                    int expectedBlanks = 0;
 
-                case 'B':
-                    setYear(Integer.parseInt(dateComponents[0]));
-                    setMonth(Integer.parseInt(dateComponents[1]));
-                    setDay(Integer.parseInt(dateComponents[2]));
-                    break;
+                    if(Integer.parseInt(getDay().replace(" ", "")) < 10)
+                        expectedBlanks++;
 
-                case 'C':
-                    setDay(Integer.parseInt(dateComponents[0]));
-                    setMonth(Integer.parseInt(dateComponents[1]));
-                    setYear(Integer.parseInt(dateComponents[2]));
-                    break;
+                    if(Integer.parseInt(getMonth().replace(" ", "")) < 10)
+                        expectedBlanks++;
 
-                case '-':
-                case '/':
-                case '.':
-                    int count = dateString.length() - dateString.replace(".", "").length();
-                    if(count != 2){
-                        //seperator isn't found twice
+                    int count = dateString.length() - dateString.replace(" ", "").length();
+                    if(count != expectedBlanks){
+                        //not enough blanks found
                         setIsValid(false);
+                        return;
                     }
                     break;
+    
+                default:
+                    //no valid option found
+                    setIsValid(false);
+                    return;
             }
         }
     }
 
 
     //getter/setter
-    public int getDay(){ return day; }
-    public void setDay(int day){ this.day = day; }
+    public String getDay(){ return day; }
+    public void setDay(String day){ this.day = day; }
 
-    public int getMonth(){ return month; }
-    public void setMonth(int month){ this.month = month; }
+    public String getMonth(){ return month; }
+    public void setMonth(String month){ this.month = month; }
 
-    public int getYear(){ return year; }
-    public void setYear(int year){ this.year = year; }
+    public String getYear(){ return year; }
+    public void setYear(String year){ this.year = year; }
 
     public boolean isValid(){ return isValid; }
     public void setIsValid(boolean isValid){ this.isValid = isValid; }
@@ -87,7 +176,7 @@ public class Date {
      */
     public String toString(String formatCode) {
         if(isValid() == false){
-            //no due to wrong input of specification
+            //no, due to wrong input of specification
             return "INVALID";
         }
 
@@ -95,9 +184,9 @@ public class Date {
         String dateString = "";
         char order = 'X', seperator = 'X';                          //default values for compiler
 
-        String day = String.format("%02d", this.day);       //default format with leading zero
-        String month = String.format("%02d", this.month);   //default format with leading zero
-        String year = this.year + "";
+        String day = this.day;
+        String month = this.month;
+        String year = this.year;
         
         //decoding formatCode
         for(int i = 0; i < formatCode.length(); i++){
@@ -106,31 +195,77 @@ public class Date {
                 case 'A':
                 case 'B':
                 case 'C':
+                    if(i != 0){
+                        //Order option in wrong position
+                        return "INVALID";
+                    }
+
                     order = c;
                     break;
 
                 case '-':
                 case '/':
                 case '.':
+                    if(i != 1){
+                        //Seperator option in wrong position
+                        return "INVALID";
+                    }
+
                     seperator = c;
                     break;
 
                 case '2':
-                    //format with 2 digits, cut off front
-                    year = String.format("%2d", this.year);
+                    if(i != 2){
+                        //Year format option in wrong position
+                        return "INVALID";
+                    }
+
+                    //only get last 2 digits
+                    year = year.substring(year.length()-2);
                     break;
 
                 case '4':
-                    //format with 4 digits, add 20 infront, get cut of anyway if format already is 4 digits
-                    year = String.format("%4d", Integer.parseInt("20" + this.year));
+                    if(i != 2){
+                        //Year format option in wrong position
+                        return "INVALID";
+                    }
+
+                    if(year.length() != 4){
+                        //since constuctor only allows length 2 and 4:
+                        //assume its this decade
+                        year = "20" + year;
+                    }
                     break;
 
                 case 'b':
-                    //format as string with length 2: 3 => " 3"
-                    day = String.format("%2d", this.day);
-                    month = String.format("%2d", this.month);
+                    if(i != 3){
+                        //Blank option in wrong position
+                        return "INVALID";
+                    }
+
+                    //replace leading zeros
+                    if(day.charAt(0) == '0')
+                        day = " " + day.charAt(1);
+
+                    if(month.charAt(0) == '0')
+                        month = " " + month.charAt(1);
+
                     break;
+
+                default:
+                    //wrong character found
+                    return "INVALID";
             }
+        }
+
+        if(formatCode.length() == 3){
+            //no leading blank option
+            //replace leading zeros
+            if(day.charAt(0) == ' ')
+                day = "0" + day.charAt(1);
+
+            if(month.charAt(0) == ' ')
+                month = "0" + month.charAt(1);
         }
 
         switch (order){
