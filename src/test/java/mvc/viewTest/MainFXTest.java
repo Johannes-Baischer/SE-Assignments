10babe6ca6e29f0613e01fbfbc89779f053ab009
Johannes-Baischer <Johannes.Baischer@gmail.com>
2022-12-17 20:38:49 +0100

    20221217 JavaFX Test setup
     - using https://github.com/TestFX/TestFX

--- src/test/java/mvc/viewTest/MainFXTest.java
+++ src/test/java/mvc/viewTest/MainFXTest.java
@@ -1,58 +1,41 @@
 package mvc.viewTest;
 
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.util.Arrays;
-import java.util.Map;
-
-import javax.swing.JPanel;
-
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.stage.Stage;
-import mvc.view.*;
-import mvc.model.*;
-import mvc.controller.*;
-
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.testfx.api.FxRobot;
+import org.testfx.assertions.api.Assertions;
+import static org.junit.jupiter.api.Assertions.*;
+import org.testfx.framework.junit5.ApplicationExtension;
+import org.testfx.framework.junit5.Start;
 
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import mvc.view.MainFX;
+
+@ExtendWith(ApplicationExtension.class)
 class MainFXTest {
+
     private MainFX main;
 
-
-    @BeforeEach
-    void setUp() throws InterruptedException {
-        Thread thread = new Thread(new Runnable() {
-
-            @Override
-            public void run() {
-                new JPanel(); // Initializes the JavaFx Platform
-                Platform.runLater(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        try { //initialize app
-                            main = new MainFX();
-                            main.start(new Stage());
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                        }
-                    }
-                });
-            }
-        });
-        thread.start();// Initialize the thread
-        Thread.sleep(10000); // Time to use the app, with out this, the thread
+    /**
+     * Will be called with {@code @Before} semantics, i. e. before each test method.
+     *
+     * @param stage - Will be injected by the test runner.
+     */
+    @Start
+    private void start(Stage stage) {
+        main = new MainFX();
+        
+        assertDoesNotThrow(() -> main.start(stage));
     }
 
+    /**
+     * @param robot - Will be injected by the test runner.
+     */
     @Test
-    void test() {
-        
+    void should_contain_addbutton(FxRobot robot) {
+        Assertions.assertThat(robot.lookup(".button").queryButton()).hasText("Add");
     }
 }
